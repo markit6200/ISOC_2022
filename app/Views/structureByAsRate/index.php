578f1429ff200bd10df5be05d1560e8e8d19b414
eakkasit <eakkasit>
2022-10-02 17:06:07 +0700

    update link add

--- app/Views/structureByAsRate/index.php
+++ app/Views/structureByAsRate/index.php
@@ -46,7 +46,7 @@
                                                 <div class="form-group mb-0">
                                                     <a style="    float: right;
                                             margin-top: -22px;
-                                            margin-right: -7px;" href="<?php echo base_url('structure-assistant-rate/form') ?>" data-controller="button" data-turbo="true" class="btn  btn-default" type="submit" form="post-form" formaction="https://demo.orchid.software/example-fields/buttonClickProcessing">
+                                            margin-right: -7px;" href="<?php echo base_url('StructureByAssistRate/form') ?>" data-controller="button" data-turbo="true" class="btn  btn-default" type="submit" form="post-form" formaction="https://demo.orchid.software/example-fields/buttonClickProcessing">
 
 
                                                         <i class="mdi mdi-plus-circle-outline"></i>&nbsp;เพิ่มตำแหน่ง
