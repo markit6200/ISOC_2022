85f47d5f8905503f596fdc82b493e195f1c0a2e5
eakkasit <eakkasit>
2022-10-22 14:23:11 +0700

    update script

--- app/Views/partials/head-css.php
+++ app/Views/partials/head-css.php
@@ -1,8 +1,8 @@
 <!-- Bootstrap Css -->
-<link href="/assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+<link href="<?php echo base_url("assets/css/bootstrap.min.css") ?>" id="bootstrap-style" rel="stylesheet" type="text/css" />
 <!-- Icons Css -->
-<link href="/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo base_url("assets/css/icons.min.css") ?>" rel="stylesheet" type="text/css" />
 <!-- App Css-->
-<link href="/assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+<link href="<?php echo base_url("assets/css/app.min.css") ?>" id="app-style" rel="stylesheet" type="text/css" />
 
-<link href="/assets/css/custom.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo base_url("assets/css/custom.css") ?>" rel="stylesheet" type="text/css" />
